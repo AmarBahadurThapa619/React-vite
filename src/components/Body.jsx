382b54f02bf69d01e64376d91b992b6488d8c03f
Amar Thapa <thapaamar9867@gmail.com>
2026-06-08 21:56:58 +0545

    Practice UseEffect and fetch api

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,13 +1,38 @@
-import ProductCard from "./ProductCard";
+import { useEffect, useState } from "react";
+// import { products } from "../constants/constant.js";
 import '../styles/Body.css';
-import {products} from "../constants/constant.js";
+import ProductCard from "./ProductCard";
 
 function Body(){
-    console.log(products);
+    // const [productList, setProductList] = useState(products);
+    const [productsList, setProductsList] = useState([]);
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    async function fetchData(){
+        const data = await fetch("https://fakestoreapi.com/products");
+        const jsonData = await data.json();
+        console.log(jsonData, "jsonData");
+        setProductsList(jsonData);
+    }
+
+    function handleTopRatedProducts(){
+        const topRatedProducts = productsList.filter((product) => product.rating.rate >= 4);
+        console.log(topRatedProducts);
+        setProductsList(topRatedProducts);
+    }
+
     return(
         <main className="product-cards">
+            <input type="text" />
+            <div>
+                <button onClick={handleTopRatedProducts}>Top Rated Products</button>
+            </div>
+
             {
-                products.map((product) =>
+                productsList.map((product) =>
                     <ProductCard data = {product} key={product.id} />
             )}
         </main>
